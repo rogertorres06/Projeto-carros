b5c479ed6747ef38310cd78019c778bf85a2ce79
rogertorres06 <62028151+rogertorres06@users.noreply.github.com>
2022-05-03 20:16:29 -0300

    adicionando pasta

---
+++ js/script.js
@@ -0,0 +1,111 @@
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times')
+  navbar.classList.remove('active')
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active')
+  } else {
+    document.querySelector('.header').classList.remove('active')
+  }
+}
+
+window.onload = () => {
+  menu.classList.remove('fa-times')
+  navbar.classList.remove('active')
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active')
+  } else {
+    document.querySelector('.header').classList.remove('active')
+  }
+}
+
+document.querySelector('.home').onmousemove = e => {
+  document.querySelectorAll('.home-parallax').forEach(elm => {
+    let speed = elm.getAttribute('data-speed')
+
+    let x = (window.innerWidth - e.pageX * speed) / 90
+    let y = (window.innerHeight - e.pageY * speed) / 90
+
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`
+  })
+}
+document.querySelector('.home').onmouseleave = e => {
+  document.querySelectorAll('.home-parallax').forEach(elm => {
+    elm.style.transform = `translateX(0px) translateY(0px)`
+  })
+}
+
+var swiper = new Swiper(".veiculos-slider", {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".featured-slider", {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    zoom: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
